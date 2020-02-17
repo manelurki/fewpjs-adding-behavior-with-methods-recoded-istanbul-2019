a58fe0240567ab48e87f6f210592d634cb9f1570
manel turki <menelturki@gmail.com>
2020-02-17 19:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
+  }
+
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
+  }
+
+ 
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
+  }
+
+  speak() {
+    if (this.sex == "male") {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
+    }
+  }
+}
